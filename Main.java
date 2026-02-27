4e8a1bc5e974b63f8e141d9d58c925f961c47706
reasearchmania112-ai <reasearchmania112@gmail.com>
2026-02-28 01:15:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("This is test java Program");
+        System.out.println("This is output from java program");
+    }
+}
